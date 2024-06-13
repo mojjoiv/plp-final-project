c9d0143efd8e352e801c7f934a4c41873ea7e529
mojjoiv <daltonongeche@gmail.com>
2024-06-13 18:14:46 +0300

    added a sign up and firebase page

--- src/App.js
+++ src/App.js
@@ -1,63 +1,44 @@
-import axios from "axios";
-import { createContext, useState } from "react";
-import InputForm from "./components/InputForm";
-import QrCode from "./components/QrCode";
-
-export const InputContext = createContext();
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
+import PersonalDetails from './components/PersonalDetails';
+import QRCodePage from './components/QRCodePage';
 
 function App() {
-  const [inputValue, setInputValue] = useState({
-    url: '',
-    color: ''
-  });
-  const [response, setResponse] = useState('');
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  const config = {
-    headers: { Authorization: 'Bearer b77f1ac0-28ca-11ef-8d4b-6b8bb582f73b' }
-  }
-  const bodyParameters = {
-    "colorDark": inputValue.color,
-    "qrCategory": "url",
-    "text": inputValue.url
-  }
-  const getQrCode = async () => {
-    try {
-      setLoading(true);
-      const res = await axios.post(
-        'https://qrtiger.com/api/qr/static',
-        bodyParameters,
-        config
-      );
-      setResponse(res.data.url);
-    } catch(err) {
-      setError(err);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  const value = {
-    inputValue,
-    setInputValue,
-    getQrCode,
-    response,
-    loading,
-    error
-  }
-
   return (
-    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen pt-24 md:pt-80 px-2">
-      <div className="container mx-auto max-w-4xl bg-white rounded-md shadow">
-        <div className="md:grid md:grid-cols-3">
-          <InputContext.Provider value={value}>
-            <InputForm />
-            <QrCode />
-          </InputContext.Provider>
-        </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/details">
+            <PersonalDetails />
+          </Route>
+          <Route path="/qr">
+            <QRCodePage />
+          </Route>
+          <Route path="/">
+            <h2>Welcome to the QR Code Generator App</h2>
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
